4b21161530b864fb7687c8e53050da13edb93d1d
Butawantemi <japhetbuta@gmail.com>
2024-10-16 18:56:34 +0300

    GraphQL create server

---
+++ dist/index.js
@@ -0,0 +1,44 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+const typeDefs = `#graphql
+    type Book {
+        title: String,
+        author: String
+    }
+
+    type Query {
+        books: [Book]}
+`;
+// Data Set
+const books = [
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowling'
+    },
+    {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton'
+    },
+    {
+        title: 'The Da Vinci Code',
+        author: 'Dan Brown'
+    }
+];
+// Resolvers
+const resolvers = {
+    Query: {
+        books: () => books,
+    }
+};
+// Apollo Server 
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+// 
+const { url } = await startStandaloneServer(server, {
+    listen: {
+        port: 4000,
+    }
+});
+console.log(`Server ready at ${url}`);
