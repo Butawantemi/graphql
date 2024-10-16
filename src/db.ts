2ddf573ee85e280821f51f84f6e30d29c9d38be5
Butawantemi <japhetbuta@gmail.com>
2024-10-16 20:04:58 +0300

    data set

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,12 @@
-import { platform } from "os"
-import { title } from "process"
 
-const Games = [
+let games = [
     {id: 1, title: 'The Legend of Zelda: Breath of the Wild', platform: ['Nintendo Switch', 'Wii U']},
     {id: 2, title: 'Super Mario Odyssey', platform: ['Nintendo Switch']},
     {id: 3, title: 'The Witcher 3: Wild Hunt', platform: ['PlayStation 4', 'Xbox one']},
     {id: 4, title: 'The Last of Us Part II', platform: ['PlayStation 4']},
     {id: 5, title: 'Red Dead Redemption 2', platform: ['PlayStation 4', 'Xbox one']}
+];
+let authors = [
+    {id: 1, name: 'Shigeru Miyamoto', verified: true},
+    {id: 2, name: 'Eiji Aonuma'}
 ]
