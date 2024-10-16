85a610381f7da045115b790b3f27f58907aeb98f
Butawantemi <japhetbuta@gmail.com>
2024-10-16 17:47:52 +0300

    GraphQL schema

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,12 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 
-const typeDefs = `#graphql`
+const typeDefs = `#graphql
+    type Book {
+        title: String,
+        author: String
+    }
+
+    type Query {
+        books: [Book]}
+`;
