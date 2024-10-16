b63f4e937ce9c2650d5e8c9ef8af84bce076bcd7
Butawantemi <japhetbuta@gmail.com>
2024-10-16 19:56:44 +0300

    data set

--- src/db.ts
+++ src/db.ts
@@ -3,5 +3,6 @@ import { title } from "process"
 
 const Games = [
     {id: 1, title: 'The Legend of Zelda: Breath of the Wild', platform: ['Nintendo Switch', 'Wii U']},
+    {id: 2, title: 'Super Mario Odyssey', platform: ['Nintendo Switch']},
     
 ]
