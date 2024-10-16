c22b05ddb3eab2861f2d9b70a1e9a153f5bbf24e
Butawantemi <japhetbuta@gmail.com>
2024-10-16 20:01:03 +0300

    data set

--- src/db.ts
+++ src/db.ts
@@ -5,5 +5,6 @@ const Games = [
     {id: 1, title: 'The Legend of Zelda: Breath of the Wild', platform: ['Nintendo Switch', 'Wii U']},
     {id: 2, title: 'Super Mario Odyssey', platform: ['Nintendo Switch']},
     {id: 3, title: 'The Witcher 3: Wild Hunt', platform: ['PlayStation 4', 'Xbox one']},
-    
+    {id: 4, title: 'The Last of Us Part II', platform: ['PlayStation 4']},
+    {id: 5, title: 'Red Dead Redemption 2', platform: ['PlayStation 4', 'Xbox one']}
 ]
