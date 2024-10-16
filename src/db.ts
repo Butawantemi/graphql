d18707e8c86262ea21c2d6f3fa8def46bc70e26b
Butawantemi <japhetbuta@gmail.com>
2024-10-16 20:10:42 +0300

    data set

--- src/db.ts
+++ src/db.ts
@@ -15,5 +15,5 @@ let authors = [
 
 let reviews = [
     {id: 1, rating: 9, content: 'A great game', author_id: 1, game_id: 2 },
-    {id: 1, rating: 9, content: 'A great game', author_id: 1, game_id: 2 }
+    {id: 2, rating: 7, content: 'well', author_id: 4, game_id: 3 }
 ]
