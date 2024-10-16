70c610d73718d027edbe951490c424724091106f
Butawantemi <japhetbuta@gmail.com>
2024-10-16 20:19:59 +0300

    data set

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,3 @@
-
 let games = [
     {id: 1, title: 'The Legend of Zelda: Breath of the Wild', platform: ['Nintendo Switch', 'Wii U']},
     {id: 2, title: 'Super Mario Odyssey', platform: ['Nintendo Switch']},
