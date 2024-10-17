7f984d50d7ff67a43f92f5f36c8fce2eb0558b00
Butawantemi <japhetbuta@gmail.com>
2024-10-17 10:27:17 +0300

    resolvers

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,19 @@ import  { typeDefs } from './schema.js';
 // Resolvers
 const resolvers = {
     Query:  {
-        Games: () => db.games,
-        Game: (_: any, args: { id: string; }) => {
+        games: () => db.games,
+        game: (_: any, args: { id: string; }) => {
             return db.games.find(game => game.id === Number(args.id));
         },
-        Authors: () => db.authors,
-        Author: (_: any, args: { id: string; }) => {
+        authors: () => db.authors,
+        author: (_: any, args: { id: string; }) => {
             return db.authors.find(author => author.id === Number(args.id));
         },
-        Reviews: () => db.reviews,
-        Review: (_: any, args: { id: string; }) => {
+        reviews: () => db.reviews,
+        review: (_: any, args: { id: string; }) => {
             return db.reviews.find(review => review.id === Number(args.id));
         },
-        game: {
+        Game: {
             Reviews: (parent: any) => {
                 return db.reviews.filter((r) => r.game_id === parent.id)
             }
