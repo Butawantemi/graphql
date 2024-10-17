9f833a63bbbefab5cff7160109a219a20e9fc2ee
Butawantemi <japhetbuta@gmail.com>
2024-10-17 12:44:06 +0300

    resolvers

--- src/index.ts
+++ src/index.ts
@@ -46,6 +46,15 @@ const resolvers = {
         deleteGame: (_: any, args: { id: string } ) => {
            db.games = db.games.filter((t) => t.id !== Number(args.id));
            return db.games
+        },
+        addGame: (_: any, args: { game: any}) => {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 1000).toString()
+            }
+
+            db.games.push(game)
+            return game;
         }
     }
 }
