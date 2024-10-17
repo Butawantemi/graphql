588824ecdc410466d6a5932d6c34f71f2b7151ab
Butawantemi <japhetbuta@gmail.com>
2024-10-17 11:19:59 +0300

    resolvers

--- src/schema.ts
+++ src/schema.ts
@@ -28,6 +28,12 @@ export const typeDefs = `#graphql
         author(id: ID!): Author
     }
     type Mutation {
+        addGame(game: GameInput): Game
         deleteGame(id: ID!): [Game]
+    }
+
+    input GameInput {
+        title: String!
+        platform: [String!]!
     }
 `
